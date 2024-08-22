2465a902cf39d768c89e1da0d290eab4a237baec
ESCatana <43874746+ESCat-lab@users.noreply.github.com>
2024-08-22 16:14:32 +0100

    Debug

--- Assets/Codes/BrushRenderer.cs
+++ Assets/Codes/BrushRenderer.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -7,69 +9,52 @@ public class BrushRenderer : MonoBehaviour
 {
     [SerializeField]
     Mesh referenceMesh;
+
+    Vector3[] tempVertexCount;
+    Vector3[] tempVertices;
+    Vector3[] tempNormals;
+    Vector2[] tempUvs;
+    List<int> tempIndices;
+
     void OnEnable () 
     {
-		var mesh = new Mesh { name = "Procedural Mesh" };
-        //mesh.vertices = new Vector3[] { Vector3.zero, Vector3.right, Vector3.up };
-        Vector3[] tempVertices = new Vector3[referenceMesh.vertexCount * 2];
-        Vector3[] tempNormals = new Vector3[tempVertices.Length];
-        Vector2[] tempUvs = new Vector2[tempVertices.Length];
-        int[] tempIndices = new int[tempVertices.Length/4 * 6];
+		    var mesh = new Mesh { name = "Procedural Mesh" };
+        tempVertexCount = new Vector3[]{referenceMesh.vertices[0], referenceMesh.vertices[1], referenceMesh.vertices[2], referenceMesh.vertices[3]};
+        tempVertices = new Vector3[tempVertexCount.Length * 2];
+        tempNormals = new Vector3[tempVertices.Length];
+        tempUvs = new Vector2[tempVertices.Length];
+        tempIndices = new List<int>();
 
-        for(int i = 0; i < referenceMesh.vertexCount; i++)
+        for(int i = 0; i < tempVertexCount.Length; i++)
         {
           if(i % 2 == 0)
           {
+            Vector3 tangent = Vector3.Cross(tempVertexCount[(i+1)%tempVertexCount.Length], tempNormals[i]).normalized;
 
-            Vector3 tangent;
+            tempVertices[i] = tempVertexCount[i] +  tangent/10;
+            tempVertices[i + 1] = tempVertexCount[i] - tangent/10;
+            //Debug.Log("Vertice " + i + " & " + (i+1) + ":         " + tempVertices[i] + " , " + tempVertices[i + 1]);
 
-            Vector3 c1 = Vector3.Cross(referenceMesh.normals[i], new Vector3(0.0f, 0.0f, 1.0f));
-            Vector3 c2 = Vector3.Cross(referenceMesh.normals[i], new Vector3(0.0f, 1.0f, 0.0f));
-
-            if( c1.magnitude > c2.magnitude )
-            {
-              tangent = c1;
-            }
-            else
-            {
-              tangent = c2;
-            }
-
-            tempVertices[i] = referenceMesh.vertices[i] +  tangent.normalized/10;
-            tempVertices[i + 1] = referenceMesh.vertices[i] - tangent.normalized/10;
-            Debug.Log("Vertice " + i + " & " + (i+1) + ":         " + tempVertices[i] + " , " + tempVertices[i + 1]);
-
-            tempNormals[i] = referenceMesh.normals[i];
-            tempNormals[i + 1] = referenceMesh.normals[i];
+            tempNormals[i] = Vector3.back;
+            tempNormals[i + 1] = Vector3.back;
             //Debug.Log("Normal " + i + " & " + (i+1) + ":           " + tempNormals[i] + " , " + tempNormals[i + 1]);
 
-            tempUvs[i] = Vector3.up; 
-            tempUvs[i + 1] = Vector3.down; 
+            //tempUvs[i] = Vector3.up; 
+            //tempUvs[i + 1] = Vector3.down; 
             //Debug.Log("Uv " + i + " & " + (i+1) + ":            " + tempUvs[i] + " , " + tempUvs[i + 1]);
-            
+
+            tempIndices.Add(i);
+            tempIndices.Add(i+1);
+            tempIndices.Add(i+2);
+
+            tempIndices.Add(i+2);
+            tempIndices.Add(i+1);
+            tempIndices.Add(i);
           }        
         }
-
-        for(int i = 0; i < tempIndices.Length / 3; i++)
-        {
-          if(i % 2 == 0)
-          {
-            tempIndices[i] = i;
-            tempIndices[i + 1] = i + 1;
-            tempIndices[i + 2] = i + 2;
-          }
-          else
-          {
-            tempIndices[i + 2] = i;
-            tempIndices[i + 1] = i + 1;
-            tempIndices[i] = i + 2;
-          }
-          Debug.Log("Indices " + i + " & " + (i+1) + " & " + (i+2) + ":        " + tempIndices[i] + " , " + tempIndices[i + 1] + " , "+ tempIndices[i + 2]);
-        }
-
         
         mesh.vertices = tempVertices;
-        mesh.triangles = tempIndices;
+        mesh.triangles = tempIndices.ToArray();
         mesh.normals = tempNormals;
         mesh.uv = tempUvs;
 
@@ -77,4 +62,24 @@ public class BrushRenderer : MonoBehaviour
 
         GetComponent<MeshFilter>().mesh = mesh;
 	}
+    void FixedUpdate() 
+    {
+      Color color = new Color(0.0f, 0.0f, 1.0f);
+      Color color2 = new Color(0.0f, 1.0f, 1.0f);
+      Vector3 c0 = this.transform.position + referenceMesh.vertices[0];
+      Vector3 c1 = this.transform.position + referenceMesh.vertices[1];
+      Vector3 c2 = this.transform.position + referenceMesh.vertices[2];
+      Vector3 c3 = this.transform.position + referenceMesh.vertices[3];
+
+      Vector3 tangent = Vector3.Cross(c0 - c1, tempNormals[0]).normalized;
+
+      Debug.DrawLine(c0, c1, color);
+      Debug.DrawLine(c0, c2, color);
+      Debug.DrawLine(c1, c3, color);
+      Debug.DrawLine(c2, c3, color);
+
+      Debug.DrawLine(c0, c0 + tangent, color2);
+      //Debug.Log("Tangent " + tangent);
+      //Debug.Log("c0 " + c0);
+    }
 }
