367d6b435f488d62a92ba5ec364e2e5fbb95984b
ESCatana <43874746+ESCat-lab@users.noreply.github.com>
2024-09-11 16:53:32 +0100

    Planes Oriented Correctly

--- Assets/Codes/BrushRenderer.cs
+++ Assets/Codes/BrushRenderer.cs
@@ -11,16 +11,16 @@ public class BrushRenderer : MonoBehaviour
     [SerializeField]
     Mesh referenceMesh;
     [SerializeField]
-    float planeSize = 0.1f;
+    float planeSize = 0.05f;
 
-    List<Vector3> CreatePlaneVertices(Vector3 spawnPosition, float planeSize)
+    List<Vector3> CreatePlaneVertices(Vector3 spawnPosition, Vector3 spawnNormal, float planeSize)
     {
       List<Vector3> tempVertices = new List<Vector3>
       {
-        spawnPosition + new Vector3(-1, 1, 0) * planeSize,
-        spawnPosition + new Vector3(-1, -1, 0) * planeSize,
-        spawnPosition + new Vector3(1, -1, 0) * planeSize,
-        spawnPosition + new Vector3(1, 1, 0) * planeSize
+        spawnPosition + Vector3.Cross(spawnNormal.normalized, new Vector3(-1, 1, 0)) * planeSize,
+        spawnPosition + Vector3.Cross(spawnNormal.normalized, new Vector3(-1, -1, 0)) * planeSize,
+        spawnPosition + Vector3.Cross(spawnNormal.normalized, new Vector3(1, -1, 0)) * planeSize,
+        spawnPosition + Vector3.Cross(spawnNormal.normalized, new Vector3(1, 1, 0)) * planeSize
       };
 
       return tempVertices;
@@ -36,6 +36,25 @@ public class BrushRenderer : MonoBehaviour
       };
 
       return tempNormals;
+    }   
+    List<int> CreatePlaneIndices(Vector3 spawnNormal)
+    {
+      List<int> tempIndices = new List<int>()
+      {
+        0,
+        1,
+        2,
+        2,
+        3,
+        0
+      };
+
+      if(spawnNormal.normalized.z < 0)
+      {
+        tempIndices.Reverse();
+      }
+      
+      return tempIndices;
     }
     List<Mesh> CreatePlanes(List<Vector3> spawnPoints, List<Vector3> spawnNormals, float planeSize)
     {
@@ -44,25 +63,26 @@ public class BrushRenderer : MonoBehaviour
       for(int i = 0; i < spawnPoints.Count; i++)
       {
         Mesh tempMesh = new Mesh { name = "Procedural Mesh no " + i };
-        tempMesh.vertices = CreatePlaneVertices(spawnPoints[i], planeSize).ToArray();
+        tempMesh.vertices = CreatePlaneVertices(spawnPoints[i], spawnNormals[i], planeSize).ToArray();
         tempMesh.normals = CreatePlaneNormals(spawnNormals[i]).ToArray();
+        tempMesh.SetIndices(CreatePlaneIndices(spawnNormals[i]).ToArray(), MeshTopology.Triangles,0);
         tempMeshes.Add(tempMesh);
 
       }
       return tempMeshes;
     }
 
-    Mesh CombineMeshesCustom(Mesh overAllMesh, List<Mesh> meshes)
+    void CombineMeshesCustom(Mesh overAllMesh, Matrix4x4 transforPosition, List<Mesh> meshes)
     {
       CombineInstance[] combineInstance = new CombineInstance[meshes.Count];
 
       for(int i = 0; i < meshes.Count; i++)
       {
         combineInstance[i].mesh = meshes[i];
+        combineInstance[i].transform = transforPosition;
       }
 
       overAllMesh.CombineMeshes(combineInstance, true, true);
-      return overAllMesh;
     }
 
 
@@ -73,7 +93,7 @@ public class BrushRenderer : MonoBehaviour
       MeshFilter filter = GetComponent<MeshFilter>();
       
       List<Mesh> meshes = CreatePlanes(referenceMesh.vertices.ToList(), referenceMesh.normals.ToList(), planeSize);
-      filter.mesh = CombineMeshesCustom(overAllMesh, meshes);
-
+      CombineMeshesCustom(overAllMesh, gameObject.transform.localToWorldMatrix, meshes);
+      filter.mesh = overAllMesh;
 	  }
 }
