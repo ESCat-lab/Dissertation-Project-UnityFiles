4171a45f9c91c575b486d46b5fdccdb578be560b
ESCatana <43874746+ESCat-lab@users.noreply.github.com>
2024-09-14 15:46:17 +0100

    Script Cleanup

--- Assets/Codes/BrushRenderer.cs
+++ Assets/Codes/BrushRenderer.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using UnityEngine.Assertions.Comparers;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))] [ExecuteInEditMode]
-public class BrushRenderer : MonoBehaviour
+public class BrushRenderer : RenderFunctions
 {
     [SerializeField]
     Mesh referenceMesh;
@@ -15,132 +15,15 @@ public class BrushRenderer : MonoBehaviour
     Vector2Int XYRatio = new Vector2Int(1,1);
     [SerializeField] [Range(0.1f, 1f)]
     float planeSize = 0.5f;
-
-    [SerializeField] [Range(0, 3)]
-    int planeDirection;
     [SerializeField]
     bool flipIndices = false;
-
-    void FindDirection(Vector3 spawnNormal)
-    {
-      if(Vector3.Cross(spawnNormal, new Vector3(1, 0, 0)).magnitude >= 1 )
-      {
-        planeDirection = 0;
-        Debug.DrawLine(this.gameObject.transform.position, this.gameObject.transform.position + Vector3.Cross(spawnNormal, new Vector3(1, 0, 0)).normalized, Color.red, 10.0f);
-      }
-      if(Vector3.Cross(spawnNormal, new Vector3(0, 1, 0)).magnitude >= 1 )
-      {
-        planeDirection = 1;
-        Debug.DrawLine(this.gameObject.transform.position, this.gameObject.transform.position + Vector3.Cross(spawnNormal, new Vector3(0, 1, 0)).normalized, Color.green, 10.0f);
-      }
-      if(Vector3.Cross(spawnNormal, new Vector3(0, 0, 1)).magnitude >= 1 )
-      {
-        planeDirection = 2; 
-        Debug.DrawLine(this.gameObject.transform.position, this.gameObject.transform.position + Vector3.Cross(spawnNormal, new Vector3(0, 0, 1)).normalized, Color.blue, 10.0f);
-      }
-    }
-
-    List<Vector3> CreatePlaneVertices(int plane, Vector3 spawnPosition, Vector3 spawnNormal, float planeSize, Vector2Int XYRatio)
-    { 
-      XYRatio = new Vector2Int(Math.Clamp(XYRatio.x, 1, 10000), Math.Clamp(XYRatio.y, 1, 10000));
-      float planeSizeX = planeSize * XYRatio.x / (XYRatio.x + XYRatio.y);
-      float planeSizeY = planeSize * XYRatio.y / (XYRatio.x + XYRatio.y);
-
-      List<Vector3> tempVertices = new List<Vector3>{};
-
-      Vector3 a = Vector3.Cross(spawnNormal.normalized, Vector3.up).normalized;
-
-      tempVertices.Add(spawnPosition - (a * planeSizeX + Vector3.Cross(spawnNormal.normalized, a).normalized * planeSizeY)* planeSize);
-      tempVertices.Add(spawnPosition - (a * planeSizeX - Vector3.Cross(spawnNormal.normalized, a).normalized * planeSizeY)* planeSize);
-      tempVertices.Add(spawnPosition + (a * planeSizeX + Vector3.Cross(spawnNormal.normalized, a).normalized * planeSizeY)* planeSize);
-      tempVertices.Add(spawnPosition + (a * planeSizeX - Vector3.Cross(spawnNormal.normalized, a).normalized * planeSizeY)* planeSize);
-
-      return tempVertices;
-    }   
-    List<Vector3> CreatePlaneNormals(Vector3 spawnNormal)
-    {
-      List<Vector3> tempNormals = new List<Vector3>()
-      {
-        spawnNormal,
-        spawnNormal,
-        spawnNormal,
-        spawnNormal
-      };
-
-      return tempNormals;
-    }   
-    List<int> CreatePlaneIndices(int plane, Vector3 spawnNormal)
-    {
-      List<int> tempIndices = new List<int>()
-      {
-        0,
-        3,
-        2,
-        2,
-        1,
-        0
-      };      
-
-      if(flipIndices)
-      {
-        tempIndices.Reverse();
-      }
-      
-      return tempIndices;
-    }
-
-    List<Vector2> CreatePlaneUVs()
-    {
-      List<Vector2> tempUVs = new List<Vector2>()
-      {
-        new Vector2(0,1),
-        new Vector2(0,0),
-        new Vector2(1,0),
-        new Vector2(1,1)
-      };
-      return tempUVs;
-    }
-
-    List<Mesh> CreatePlanes(List<Vector3> spawnPoints, List<Vector3> spawnNormals, float planeSize, Vector2Int XYRatio)
-    {
-      List<Mesh> tempMeshes = new List<Mesh>();
-      //Debug.Log(spawnPoints.Count);
-      for(int i = 0; i < spawnPoints.Count; i++)
-      {
-        Mesh tempMesh = new Mesh { name = "Procedural Mesh no " + i };
-        //FindDirection(spawnNormals[i]);
-        tempMesh.vertices = CreatePlaneVertices(planeDirection, spawnPoints[i], spawnNormals[i], planeSize, XYRatio).ToArray();
-        tempMesh.normals = CreatePlaneNormals(spawnNormals[i]).ToArray();
-        tempMesh.uv = CreatePlaneUVs().ToArray();
-        tempMesh.SetIndices(CreatePlaneIndices(planeDirection, spawnNormals[i]).ToArray(), MeshTopology.Triangles,0);
-        tempMeshes.Add(tempMesh);
-
-      }
-      return tempMeshes;
-    }
-
-
-    void CombineMeshesCustom(Mesh overAllMesh, Matrix4x4 transforPosition, List<Mesh> meshes)
-    {
-      CombineInstance[] combineInstance = new CombineInstance[meshes.Count];
-
-      for(int i = 0; i < meshes.Count; i++)
-      {
-        combineInstance[i].mesh = meshes[i];
-        combineInstance[i].transform = transforPosition;
-      }
-
-      overAllMesh.CombineMeshes(combineInstance, true, true);
-    }
-
-
 
     void OnEnable () 
     {
 		  Mesh overAllMesh = new Mesh { name = "Combined Mesh" };
       MeshFilter filter = GetComponent<MeshFilter>();
       
-      List<Mesh> meshes = CreatePlanes(referenceMesh.vertices.ToList(), referenceMesh.normals.ToList(), planeSize, XYRatio);
+      List<Mesh> meshes = CreatePlanes(referenceMesh.vertices.ToList(), referenceMesh.normals.ToList(), planeSize, XYRatio, flipIndices);
       CombineMeshesCustom(overAllMesh, Matrix4x4.identity, meshes);
       filter.mesh = overAllMesh;
 	  }
--- Assets/Codes/RenderFunctions.cs
+++ Assets/Codes/RenderFunctions.cs
@@ -4,5 +4,100 @@ using UnityEngine;
 
 public class RenderFunctions : MonoBehaviour
 {
+    protected List<Vector3> CreatePlaneVertices(Vector3 spawnPosition, Vector3 spawnNormal, float planeSize, Vector2Int XYRatio)
+    { 
+      XYRatio = new Vector2Int(Mathf.Clamp(XYRatio.x, 1, 10000), Mathf.Clamp(XYRatio.y, 1, 10000));
+      float planeSizeX = planeSize * XYRatio.x / (XYRatio.x + XYRatio.y);
+      float planeSizeY = planeSize * XYRatio.y / (XYRatio.x + XYRatio.y);
+
+      List<Vector3> tempVertices = new List<Vector3>{};
+
+      Vector3 a = Vector3.Cross(spawnNormal.normalized, Vector3.up).normalized;
+
+      tempVertices.Add(spawnPosition - (a * planeSizeX + Vector3.Cross(spawnNormal.normalized, a).normalized * planeSizeY)* planeSize);
+      tempVertices.Add(spawnPosition - (a * planeSizeX - Vector3.Cross(spawnNormal.normalized, a).normalized * planeSizeY)* planeSize);
+      tempVertices.Add(spawnPosition + (a * planeSizeX + Vector3.Cross(spawnNormal.normalized, a).normalized * planeSizeY)* planeSize);
+      tempVertices.Add(spawnPosition + (a * planeSizeX - Vector3.Cross(spawnNormal.normalized, a).normalized * planeSizeY)* planeSize);
+
+      return tempVertices;
+    }   
+    
+    protected List<Vector3> CreatePlaneNormals(Vector3 spawnNormal)
+    {
+      List<Vector3> tempNormals = new List<Vector3>()
+      {
+        spawnNormal,
+        spawnNormal,
+        spawnNormal,
+        spawnNormal
+      };
+
+      return tempNormals;
+    }
+   
+    protected List<int> CreatePlaneIndices(bool flipIndices)
+    {
+      List<int> tempIndices = new List<int>()
+      {
+        0,
+        3,
+        2,
+        2,
+        1,
+        0
+      };      
+
+      if(flipIndices)
+      {
+        tempIndices.Reverse();
+      }
+      
+      return tempIndices;
+    }
+
+    protected List<Vector2> CreatePlaneUVs()
+    {
+      List<Vector2> tempUVs = new List<Vector2>()
+      {
+        new Vector2(0,1),
+        new Vector2(0,0),
+        new Vector2(1,0),
+        new Vector2(1,1)
+      };
+      return tempUVs;
+    }
+
+    protected List<Mesh> CreatePlanes(List<Vector3> spawnPoints, List<Vector3> spawnNormals, float planeSize, Vector2Int XYRatio, bool flipIndices)
+    {
+      List<Mesh> tempMeshes = new List<Mesh>();
+      //Debug.Log(spawnPoints.Count);
+      for(int i = 0; i < spawnPoints.Count; i++)
+      {
+        Mesh tempMesh = new Mesh { name = "Procedural Mesh no " + i };
+        //FindDirection(spawnNormals[i]);
+        tempMesh.vertices = CreatePlaneVertices(spawnPoints[i], spawnNormals[i], planeSize, XYRatio).ToArray();
+        tempMesh.normals = CreatePlaneNormals(spawnNormals[i]).ToArray();
+        tempMesh.uv = CreatePlaneUVs().ToArray();
+        tempMesh.SetIndices(CreatePlaneIndices(flipIndices).ToArray(), MeshTopology.Triangles,0);
+        tempMeshes.Add(tempMesh);
+
+      }
+      return tempMeshes;
+    }
+
+    protected void CombineMeshesCustom(Mesh overAllMesh, Matrix4x4 transforPosition, List<Mesh> meshes)
+    {
+      CombineInstance[] combineInstance = new CombineInstance[meshes.Count];
+
+      for(int i = 0; i < meshes.Count; i++)
+      {
+        combineInstance[i].mesh = meshes[i];
+        combineInstance[i].transform = transforPosition;
+      }
+
+      overAllMesh.CombineMeshes(combineInstance, true, true);
+    }
+
+
 
 }
